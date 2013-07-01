7b954c9c28dc878eb6db1ab8e78b93c094d12fa3
vuuvv <vuuvv@qq.com>
2013-07-01 23:53:26 +0800

    Generated the Rails 4 application

---
+++ README.md
@@ -0,0 +1,7 @@
+== TICKETEE
+
+* Tracks tickets and groups them into projects
+* Provides a way to restrict users to certain projects
+* Allows users to upload files to tickets
+* Lets users tag tickets so they're easy to find
+* Provides an API on which users can base development of their own application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketee::Application.config.secret_key_base = '3b70e4712a822837b821d364348dbe2ffbf2fb65ec573fe204edd4595d5b4ddf80dd4d5b6151fedc172b4e08c91e0d2fd3bc47645761c0f2ab52008282dc8c1b'
